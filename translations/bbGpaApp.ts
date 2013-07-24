9e63d0a1b0f71adc70f4cdc4ad98588c9b3c53e7
elliottjro <ejaiaro@gmail.com>
2013-07-24 12:54:07 -0700

    Initial Commit
    This is the initial commit for the bb gpa app

---
+++ translations/bbGpaApp.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>main</name>
+    <message>
+        <source>Picture actions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Set of the useful things to do ...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Break</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unbreak</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Hide</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Show</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>hidden</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>broken</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
